f0cf4be3316e5f4940b1088cc606049cf4eacf4d
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-28 22:33:00 +0530

    added basic structure

--- src/view/Player/Player.js
+++ src/view/Player/Player.js
@@ -3,17 +3,17 @@ import Body from "../../components/Body/Body";
 import Footer from "../../components/Footer/Footer";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useStateValue } from "../../store/stateProvider";
+import "./player.scss";
 
 function Player() {
   const [{ user }] = useStateValue();
   return (
     <div className="player">
       <div className="player__body">
-        <h1>Welcome to spotify {user?.display_name}</h1>
         <Sidebar />
         <Body />
-        <Footer />
       </div>
+      <Footer />
     </div>
   );
 }
