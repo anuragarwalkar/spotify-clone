a1f3ebd9b031be9ac8e50eba0fe3ffbce4597354
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-28 20:17:46 +0530

    added fitter sidebar and body component

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sidebar.scss";
+
+function Sidebar() {
+  return <div className="sidebar">I'm sidebar</div>;
+}
+
+export default Sidebar;
--- src/view/Player/Player.js
+++ src/view/Player/Player.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Body from "../../components/Body/Body";
+import Footer from "../../components/Footer/Footer";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import { useStateValue } from "../../store/stateProvider";
 
 function Player() {
   const [{ user }] = useStateValue();
-  return <div className="player">Welcome to spotify {user.display_name}</div>;
+  return (
+    <div className="player">
+      <div className="player__body">
+        <h1>Welcome to spotify {user?.display_name}</h1>
+        <Sidebar />
+        <Body />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default Player;
