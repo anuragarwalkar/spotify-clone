26e104d1abf67d0d523d57389e836f18387587b8
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-28 23:23:36 +0530

    added mui icons and static sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,31 @@
+import HomeIcon from "@mui/icons-material/Home";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
+import SidebarOption from "../SidebarOption/SidebarOption";
 import "./sidebar.scss";
 
 function Sidebar() {
-  return <div className="sidebar">I'm sidebar</div>;
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify Logo"
+      />
+
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLIST</strong>
+      <hr />
+
+      <SidebarOption title="Hip hop" />
+      <SidebarOption title="Rock" />
+      <SidebarOption title="Rmb" />
+    </div>
+  );
 }
 
 export default Sidebar;
