062e8632cad15983ef71640fc22f005ca94e2bdc
anuragarwalkar <anuragarwalkar@ehrdata.com>
2021-11-28 19:47:27 +0530

    added initial confid to set user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 import "./App.scss";
 import { getTokenFromUrlAndReset } from "./config/spotify";
+import { actionTypes } from "./store/reducer";
+import { useStateValue } from "./store/stateProvider";
 import LoginScreen from "./view/Auth/LoginScreen";
 import Player from "./view/Player/Player";
 
@@ -9,21 +11,24 @@ const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
 
-  const initializeSpotifyAccess = async () => {
+  const initializeSpotifyAccess = useCallback(async () => {
     const { access_token: accessToken = null } = getTokenFromUrlAndReset();
 
     if (accessToken) {
       setToken(accessToken);
       spotify.setAccessToken(accessToken);
-      const userProf = await spotify.getMe();
-      console.log("userProf:", userProf);
+      const user = await spotify.getMe();
+      dispatch({ type: actionTypes.SET_USER, user });
     }
-  };
+  }, [dispatch]);
+
+  console.log("user:", user);
 
   useEffect(() => {
     initializeSpotifyAccess();
-  }, []);
+  }, [initializeSpotifyAccess]);
 
   return <div className="app">{token ? <Player /> : <LoginScreen />}</div>;
 }
--- src/store/stateProvider.js
+++ src/store/stateProvider.js
@@ -1,11 +1,13 @@
-import { createContext, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 
-export const StateContext = createContext();
+export const RootContext = createContext();
 
 export const StateProvider = ({ initialState, reducer, children }) => {
   return (
-    <StateContext.Provider value={useReducer(reducer, initialState)}>
+    <RootContext.Provider value={useReducer(reducer, initialState)}>
       {children}
-    </StateContext.Provider>
+    </RootContext.Provider>
   );
 };
+
+export const useStateValue = () => useContext(RootContext);
